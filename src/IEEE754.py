1b7f1b9838ef64177668ef91d9ad42cdd64a0ee2
janderudder <39810873+janderudder@users.noreply.github.com>
2022-02-15 14:16:00 +0100

    feat - add special values (inf,nan) and normalization concept
    Not much tested yet

--- src/IEEE754.py
+++ src/IEEE754.py
@@ -1,3 +1,4 @@
+from math import inf, nan
 from typing_extensions import Self
 from integral_base_convert import convert as convert_i
 from fractional_base_convert import convertString as convert_f
@@ -28,24 +29,33 @@ class IEEE754_32():
 
     def __init__(self, sign: int, biasedExponent: int, mantissa: float) -> None:
         self._sign = sign
-        self._biasedExponent = biasedExponent
+        self._exponentBiased = biasedExponent
         self._mantissa = mantissa
 
     def sign(self):
         return self._sign
 
     def exponentBiased(self):
-        return self._biasedExponent
+        return self._exponentBiased
 
     def exponentUnbiased(self):
-        return self._biasedExponent-IEEE754_32.EXPONENT_BIAS
+        return self._exponentBiased-IEEE754_32.EXPONENT_BIAS
 
     def mantissa(self):
         return self._mantissa
 
+    def isNormalized(self):
+        return not (self._exponentBiased==0 and self._mantissa!=0)
+
     def value(self):
+        if self._exponentBiased==255:
+            if self._mantissa==0:
+                return (-1)**self._sign * inf
+            return nan
+        elif self._exponentBiased==0 and self._mantissa==0:
+            return 0.0
         return (
             (-1)**self._sign
             * (2**self.exponentUnbiased())
-            * (1.0+self._mantissa)
+            * ((1.0 if self.isNormalized() else 0.0) + self._mantissa)
         )
